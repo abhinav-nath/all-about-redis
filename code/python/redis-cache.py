5ce0effb5eab2d96952196d29aa3657848854e0b
Abhinav Nath <abhinavnath@ymail.com>
2022-08-12 00:27:48 +0530

    Add a simple Python program for Redis cache

---
+++ code/python/redis-cache.py
@@ -0,0 +1,26 @@
+import redis
+
+r = redis.Redis(host='localhost', port=6379)
+
+r.set("Spain", "Madrid")
+r.set("Canada", "Ottawa")
+
+print(r.get("Spain"))  # b'Madrid'
+print(r.get("Canada"))  # b'Ottawa'
+
+print("\n")
+
+# mset() accepts a dictionary
+r.mset({"Japan": "Tokyo", "Netherlands": "Amsterdam"})
+
+print(r.get("Japan"))  # b'Tokyo'
+print(r.get("Netherlands"))  # b'Amsterdam'
+
+print("\n")
+
+if(r.exists("Russia")):
+    print(r.get("Russia"))
+else:
+    print("Cannot find the capital of Russia, adding it")
+    r.set("Russia", "Moscow")
+    print(r.get("Russia"))  # b'Amsterdam'
