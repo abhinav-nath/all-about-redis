d5f9608b610b33bfc08a380ef3fed333b71aa49e
Abhinav Nath <abhinavnath@ymail.com>
2022-08-12 00:29:50 +0530

    Add docker-compose.yml for Redis

--- code/python/redis-cache.py
+++ code/python/redis-cache.py
@@ -5,16 +5,16 @@ r = redis.Redis(host='localhost', port=6379)
 r.set("Spain", "Madrid")
 r.set("Canada", "Ottawa")
 
-print(r.get("Spain"))  # b'Madrid'
-print(r.get("Canada"))  # b'Ottawa'
+print(r.get("Spain"))  #  b'Madrid'
+print(r.get("Canada"))  #  b'Ottawa'
 
 print("\n")
 
 # mset() accepts a dictionary
 r.mset({"Japan": "Tokyo", "Netherlands": "Amsterdam"})
 
-print(r.get("Japan"))  # b'Tokyo'
-print(r.get("Netherlands"))  # b'Amsterdam'
+print(r.get("Japan"))  #  b'Tokyo'
+print(r.get("Netherlands"))  #  b'Amsterdam'
 
 print("\n")
 
@@ -23,4 +23,4 @@ if(r.exists("Russia")):
 else:
     print("Cannot find the capital of Russia, adding it")
     r.set("Russia", "Moscow")
-    print(r.get("Russia"))  # b'Amsterdam'
+    print(r.get("Russia"))  #  b'Moscow'
